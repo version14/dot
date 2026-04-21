01c3f816ea917f0470a9f73f5e494af7fb13f167
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-04-21 21:31:48 +0200

    feat(questions): Add builders for select/text/if/loop

---
+++ internal/new-test/registry/questions/select.go
@@ -0,0 +1,52 @@
+package questions
+
+// Builder
+
+type SelectBuilder struct {
+	optionQuestion *OptionQuestion
+}
+
+func Select(label, value string) *SelectBuilder {
+	return &SelectBuilder{optionQuestion: &OptionQuestion{Label: label, Value: value}}
+}
+
+func (b *SelectBuilder) Description(d string) *SelectBuilder {
+	b.optionQuestion.Description = d
+	return b
+}
+
+func (b *SelectBuilder) Multiple() *SelectBuilder {
+	b.optionQuestion.Multiple = true
+	return b
+}
+
+// Choice adds an option. next is optional (0 or 1 argument).
+func (b *SelectBuilder) Choice(label, value string, next ...*Question) *SelectBuilder {
+	opt := &Option{Label: label, Value: value}
+	if len(next) > 0 && next[0] != nil {
+		opt.Next = &Next{Question: next[0]}
+	}
+	b.optionQuestion.Options = append(b.optionQuestion.Options, opt)
+	return b
+}
+
+// ChoiceGen adds a choice that triggers a generator when selected.
+func (b *SelectBuilder) ChoiceGen(label, value, generatorID string, next ...*Question) *SelectBuilder {
+	opt := &Option{Label: label, Value: value, GeneratorID: generatorID}
+	if len(next) > 0 && next[0] != nil {
+		opt.Next = &Next{Question: next[0]}
+	}
+	b.optionQuestion.Options = append(b.optionQuestion.Options, opt)
+	return b
+}
+
+// Then sets the shared continuation taken after the question regardless of
+// which option was chosen. Required for Multiple() questions.
+func (b *SelectBuilder) Then(next *Question) *SelectBuilder {
+	b.optionQuestion.Next = &Next{Question: next}
+	return b
+}
+
+func (b *SelectBuilder) Q() *Question {
+	return &Question{OptionQuestion: b.optionQuestion}
+}
