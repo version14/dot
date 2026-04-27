aecfcf714ebd175db81ac4d610041bca1d81d100
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-04-28 01:52:09 +0200

    refactor(base_project): Fetch base project files from GitHub
    Replace inline README/.gitignore/LICENSE rendering and manual state
    writes with fetching a GitHub template repo snapshot and populating the
    virtual project state from that snapshot. Add getBaseFilesSnapshot and
    use render.PopulateStateFromSnapshot.

--- generators/base_project/generator.go
+++ generators/base_project/generator.go
@@ -1,15 +1,14 @@
 package baseproject
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/version14/dot/internal/render"
-	"github.com/version14/dot/internal/state"
 	"github.com/version14/dot/pkg/dotapi"
 )
 
 // Generator implements dotapi.Generator for base_project. It writes universal
-// project files: README.md, .gitignore, and LICENSE. It must run first because
+// project files from a GitHub template repository. It must run first because
 // later generators may modify these (e.g. typescript_base appends node_modules
 // to .gitignore).
 type Generator struct{}
@@ -29,76 +28,28 @@ func (g *Generator) Generate(ctx *dotapi.Context) error {
 		projectName = "my-project"
 	}
 
-	data := map[string]interface{}{
-		"ProjectName": projectName,
-	}
-
-	if err := writeReadme(ctx.State, data); err != nil {
-		return err
-	}
-	if err := writeGitignore(ctx.State); err != nil {
-		return err
-	}
-	if err := writeLicense(ctx.State, data); err != nil {
-		return err
-	}
-	return nil
-}
-
-func writeReadme(s *state.VirtualProjectState, data map[string]interface{}) error {
-	out, err := render.Render(readmeTmpl, data)
+	snapshot, err := getBaseFilesSnapshot()
 	if err != nil {
-		return fmt.Errorf("base_project: render README: %w", err)
+		return err
 	}
-	s.WriteFile("README.md", out, state.ContentRaw)
+
+	if err := render.PopulateStateFromSnapshot(ctx.State, snapshot); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func writeGitignore(s *state.VirtualProjectState) error {
-	s.WriteFile(".gitignore", []byte(gitignoreContent), state.ContentRaw)
-	return nil
-}
-
-func writeLicense(s *state.VirtualProjectState, data map[string]interface{}) error {
-	out, err := render.Render(licenseTmpl, data)
+func getBaseFilesSnapshot() (*render.RepoSnapshot, error) {
+	fetcher := render.NewGitHubArchiveFetcher()
+	snapshot, err := fetcher.FetchRepo(
+		context.TODO(),
+		"https://github.com/mathieusouflis/github-template.git",
+		render.FetchOptions{},
+	)
 	if err != nil {
-		return fmt.Errorf("base_project: render LICENSE: %w", err)
+		return nil, err
 	}
-	s.WriteFile("LICENSE", out, state.ContentRaw)
-	return nil
+
+	return snapshot, nil
 }
-
-const readmeTmpl = `# {{.ProjectName}}
-
-> Scaffolded with [DOT](https://github.com/version14/dot).
-
-## Getting Started
-
-See language-specific instructions in the relevant subdirectories.
-`
-
-const gitignoreContent = `# Editor / OS
-.DS_Store
-*.swp
-.idea/
-.vscode/
-
-# Build artifacts
-dist/
-build/
-out/
-*.log
-
-# Dependencies
-node_modules/
-vendor/
-`
-
-const licenseTmpl = `MIT License
-
-Copyright (c) {{.ProjectName}}
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction.
-`
