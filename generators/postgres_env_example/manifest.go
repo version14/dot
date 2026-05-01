7ba85a79301fcedac7452fb63cf4338c2e9c22cc
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2026-05-01 22:34:39 +0200

    refactor(generators): Update postgres generators

--- generators/postgres_env_example/manifest.go
+++ generators/postgres_env_example/manifest.go
@@ -4,9 +4,12 @@ import "github.com/version14/dot/pkg/dotapi"
 
 var Manifest = dotapi.Manifest{
 	Name:        "postgres_env_example",
-	Version:     "0.1.0",
+	Version:     "0.1.1",
 	Description: "Appends PostgreSQL DATABASE_URL to .env.example",
 	DependsOn:   []string{"express_server_entrypoint"},
 	Outputs:     []string{},
 	Validators:  []dotapi.Validator{},
+	PostGenerationCommands: []dotapi.Command{
+		{Cmd: "cp .env.example .env"},
+	},
 }
